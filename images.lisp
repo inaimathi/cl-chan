8e9b508bb4089a53e2f53b0dae5a0835bbbe34ae
inaimathi <inaimathi@lambda.gateway.2wire.net>
2012-07-16 21:48:19 -0400

    -Working JPG image upload and display

--- images.lisp
+++ images.lisp
@@ -41,3 +41,21 @@ Creates a preview of the given image in the folder specified by pathname."
       (write-png (imago:resize pic new-width new-height) 
 		 (merge-pathnames pic-name *preview-dir*)))
     (namestring pic-name)))
+
+;;;;;;;;;; JPGs
+(defclass jpg (image-upload) ())
+(defclass image/jpeg (jpg) ())
+(defclass image/pjpeg (jpg) ())
+
+(defmethod store-images! ((img jpg))
+  "Takes an image and a pathname. 
+Creates a preview of the given image in the folder specified by pathname."
+  (let ((pic-name (make-pathname :name (name img) :type "jpg")))
+    (copy-file (file-path img) (merge-pathnames pic-name *big-dir*))
+    (let* ((pic (read-image-file (merge-pathnames pic-name *big-dir*)))
+	   (w (ch-image:image-width pic))
+	   (h (ch-image:image-height pic)))
+      (multiple-value-bind (new-width new-height) (new-dimensions 250 w h)
+	(write-image-file (merge-pathnames pic-name *preview-dir*)
+			  (ch-image:resize-image pic new-height new-width))))
+    (namestring pic-name)))
